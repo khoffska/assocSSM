c119b7d65e11846312936366a2886d72ccc828ef
k <k@DESKTOP-1UEIL7D>
2026-02-22 16:42:58 -0500

    Use managed AWS-RunAnsiblePlaybook document to fix SSM Document error

--- ssm.tf
+++ ssm.tf
@@ -1,19 +1,14 @@
-resource "aws_ssm_document" "ansible_runbook" {
-  name          = "AnsibleRunbook"
-  document_type = "Command"
-
-  content = templatefile("${path.module}/ssm_document.json.tpl", {
-    playbook_content = jsonencode(file("${path.module}/ansible/site.yml"))
-  })
-}
-
 resource "aws_ssm_association" "ansible_association" {
-  name = aws_ssm_document.ansible_runbook.name
+  name = "AWS-RunAnsiblePlaybook"
 
   targets {
     key    = "tag:AnsibleManaged"
     values = ["true"]
   }
 
-  schedule_expression = "rate(30 minutes)" # Or whatever frequency is desired
+  parameters = {
+    playbook = file("${path.module}/ansible/site.yml")
+  }
+
+  schedule_expression = "rate(30 minutes)"
 }
