4c0bee2979501391514b2440ce946b5717a83ad4
k <k@DESKTOP-1UEIL7D>
2026-02-22 16:39:28 -0500

    Mark vpc_id as sensitive and add private_ips output

--- outputs.tf
+++ outputs.tf
@@ -1,9 +1,10 @@
-output "instance_ips" {
-  description = "Public IP addresses of the EC2 instances"
-  value       = module.ec2_instances[*].public_ip
+output "instance_private_ips" {
+  description = "Private IP addresses of the EC2 instances"
+  value       = module.ec2_instances[*].private_ip
 }
 
 output "vpc_id" {
   description = "The ID of the VPC"
   value       = data.aws_vpc.selected.id
+  sensitive   = true
 }
